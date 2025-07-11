33dd608c35454bde9652a46e599a40b2b26cc3d6
bambucha12 <q1883855@gmail.com>
2025-07-11 21:55:20 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     if (window.Telegram && Telegram.WebApp) {
     console.log("Running inside Telegram WebApp");
     console.log("Launch mode:", Telegram.WebApp.platform, Telegram.WebApp.isExpanded);
+    Telegram.WebApp.expand()
 } else {
     console.log("Not in Telegram WebApp (running in browser?)");
 }
