92aab88efbb0666750aa526d119818af811c5ff1
bambucha12 <q1883855@gmail.com>
2025-07-11 19:45:10 +0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    if (Telegram.WebApp && Telegram.WebApp.expand) {
-    Telegram.WebApp.expand();
-} else {
-    console.error("Telegram.WebApp.expand() is not available");
-}
-});
