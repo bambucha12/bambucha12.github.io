f7682b54cfd2e2db9374a8a0b3cf9753ec6d878e
bambucha12 <q1883855@gmail.com>
2025-07-11 20:00:35 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     if (window.Telegram && Telegram.WebApp) {
     console.log("Running inside Telegram WebApp");
+    console.log("Launch mode:", Telegram.WebApp.platform, Telegram.WebApp.isExpanded);
 } else {
     console.log("Not in Telegram WebApp (running in browser?)");
 }
